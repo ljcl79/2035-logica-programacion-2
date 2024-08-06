5f0d0fe6dea76147a07943cfefc774b4f23d250e
Silvia Gonzalez <devsoftwaregonzalez@gmail.com>
2024-08-06 00:16:27 -0300

    Se crean funcion para boton Intento

---
+++ app.js
@@ -0,0 +1,28 @@
+let numeroSecreto = generarNumeroSecreto();
+
+console.log(numeroSecreto);
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(typeof(numeroDeUsuario));
+    console.log(numeroSecreto);
+    console.log(numeroDeUsuario);
+    console.log(typeof(numeroSecreto));
+    console.log(numeroDeUsuario === numeroSecreto);
+    return;
+}
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;  
+}
+
+
+asignarTextoElemento('h1', 'Juego del numero secreto!');
+asignarTextoElemento('p', 'Indica un numero del 1 al 10');
+
